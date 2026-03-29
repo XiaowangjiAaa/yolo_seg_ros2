72fe9871691cb3ae7dc0ce4ec1f570cd28445ffb
xiaowangji <2426640575@qq.com>
2026-03-30 09:22:18 +1100

    Initial commit of yolo_seg_ros2

--- launch/yolo_seg.launch.py
+++ launch/yolo_seg.launch.py
@@ -1,3 +1,21 @@
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+#
+#
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='yolo_seg_ros2',
+#             executable='yolo_seg_node',
+#             name='yolo_seg_node',
+#             output='screen',
+#             parameters=[
+#                 {'input_topic': '/camera/rgb/image_raw'},
+#                 {'output_topic': '/yolo_result'},
+#             ]
+#         )
+#     ])
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -10,7 +28,7 @@ def generate_launch_description():
             name='yolo_seg_node',
             output='screen',
             parameters=[
-                {'input_topic': '/camera/rgb/image_raw'},
+                {'input_topic': '/ascamera/camera_publisher/rgb0/image'},
                 {'output_topic': '/yolo_result'},
             ]
         )
